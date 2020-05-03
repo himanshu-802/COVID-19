bd1fc7e1c29e438cab52f76822bae38681dc23e6
Himanshu Rathore <36191619+himanshu-802@users.noreply.github.com>
2020-05-03 13:09:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,61 @@
   include 'Links/links.php';
   include 'CSS/style.php';
   ?>
+
+
+
+  <script type="text/javascript">
+	
+	function func(){
+	       $.get("https://api.covid19api.com/summary",
+               
+               function(data){
+               	// console.log(data['Countries'].length);
+
+               	var tbval=document.getElementById('tbval');
+               	var i;
+               	for( i=1;i < (data['Countries'].length);i++){
+
+               		var x=tbval.insertRow();
+               		x.insertCell(0);
+               		tbval.rows[i].cells[0].innerHTML= data['Countries'][i-1]['Country'];
+               		tbval.rows[i].cells[0].style.background="#7a4a91";
+               	    tbval.rows[i].cells[0].style.color="white";
+               	    x.insertCell(1)
+               	    tbval.rows[i].cells[1].innerHTML= data['Countries'][i-1]['TotalConfirmed'];
+               		tbval.rows[i].cells[1].style.background="#7a4a91";
+                    
+                    x.insertCell(2);
+               		tbval.rows[i].cells[2].innerHTML= data['Countries'][i-1]['TotalRecovered'];
+               		tbval.rows[i].cells[2].style.background="#7a4a91";
+                   
+                    x.insertCell(3);
+               		tbval.rows[i].cells[3].innerHTML= data['Countries'][i-1]['TotalDeaths'];
+               		tbval.rows[i].cells[3].style.background="#7a4a91";
+
+                    x.insertCell(4);
+               		tbval.rows[i].cells[4].innerHTML= data['Countries'][i-1]['NewConfirmed'];
+               		tbval.rows[i].cells[4].style.background="#7a4a91";
+               		
+               		x.insertCell(5);
+               		tbval.rows[i].cells[5].innerHTML= data['Countries'][i-1]['NewRecovered'];
+               		tbval.rows[i].cells[5].style.background="#7a4a91";
+
+                    x.insertCell(6);
+               		tbval.rows[i].cells[6].innerHTML= data['Countries'][i-1]['NewDeaths'];
+               		tbval.rows[i].cells[6].style.background="#7a4a91";	
+               	}
+
+               }
+	       	);
+	}
+</script>
 </head>
-<body>
+
+
+<body onload="func()">
+
+
 <nav class="navbar navbar-expand-lg nav_style p-3">
   <a class="navbar-brand pl-5" href="#">Covid-19</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,7 +74,7 @@
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">about</a>
+        <a class="nav-link" href="sectionid">about</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Symptoms</a>
@@ -60,12 +113,30 @@
 	</div>
 </div>
 <!-- **************** corona latest  updates **************** -->
-
-<section class="corona_update">
+<br>
+<br><br>
+<section class="container-fluid corona_update">
 	
 	<div class="mb-3">
 		<h3 class="text-uppercase  text-center">covid-10 updates</h3>
-		<div class="d-flex justify-content-around align-items-center">
+
+
+		<div class=" table-responsive">
+
+			<table class=" table table-bordered table-stripped text-center " id="tbval">
+			<tr>
+					<th>Country</th>
+					<th>TotalConfirmed</th>
+					<th>TotalRecovered</th>
+					<th>TotalDeaths</th>
+					<th>NewConfirmed</th>
+					<th>NewRecovered</th>
+					<th>NewDeaths</th>
+				</tr>
+			</table>
+			
+		</div>
+		<!-- <div class="d-flex justify-content-around align-items-center">
 			<div>
 				<h1 class="count">123344</h1>
 				<p> Passengera scrrened at airport</p>
@@ -82,7 +153,7 @@
 				<h1 class="count">123344</h1>
 				<p> Death</p>
 			</div>
-		</div>
+		</div> -->
 		
 	</div>
 </section>
@@ -90,7 +161,7 @@
 
 <!-- ****************** about section *************** -->
 
-<div class="container-fluid sub_section pt-5 pb-5 id="aboutid">
+<div class="container-fluid sub_section pt-5 pb-5 id="sectionid">
 	<div class="section_header text-center mb-5 mt-4">
 		<h1> About Covid-19</h1>
 	</div>
@@ -321,6 +392,8 @@
 	</div>
 
 </div>
+
+
 </body>
 </html>
 
